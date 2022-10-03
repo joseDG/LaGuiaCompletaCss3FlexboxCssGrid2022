d3958157f3ae2ef47ce269c356c99b727caa7c01
joseDG <josethp1@gmail.com>
2022-10-03 09:29:35 -0500

    Finalizacion de Web Cafeteria

--- 17-SitioCafeteriaSaas/gulpfile.js
+++ 17-SitioCafeteriaSaas/gulpfile.js
@@ -1,21 +1,63 @@
-const { src, dest, watch} = require('gulp');
+const { src, dest, watch, series} = require('gulp');
+
+//CSS Y SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
 
-function css(){
+// Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+function css(done){
     //copilar sass
     //paso: 1 - identificar archivo, 2 - Compilar, 3 - Guardar el .css
     src('src/scss/app.scss')
         .pipe( sass() )
-        .pipe(postcss([ autoprefixer() ]))
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe( sourcemaps.write('.'))
         .pipe(dest('build/css'))
+        
     done();
 }
 
+function imagenes() {
+    return src('src/img/**/*')
+        .pipe( imagemin({ optimizationLevel: 3 }) )
+        .pipe( dest('build/img') )
+}
+
+function versionWebp() {
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe( webp( opciones ) )
+        .pipe( dest('build/img') )
+}
+function versionAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe( avif( opciones ) )
+        .pipe( dest('build/img'))
+}
+
 function dev(){
-    watch('src/scss/app.scss', css);
+    watch('src/scss/**/*.scss', css);
+    watch( 'src/img/**/*', imagenes );
 }
 
 exports.css = css;
 exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series( imagenes, versionWebp, versionAvif, css, dev  );
+
+//series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente
+//parallel - Todas inician al mismo tiempo
